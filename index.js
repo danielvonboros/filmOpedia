894e3c24adce03eb98efe6deb34f3f94d653c40a
danielvonboros <dboros40@gmail.com>
2021-05-27 18:07:48 +0200

    connection with MongoDB Atlas
    index.js
    -add connectivity to mongoDB Atlas online database.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { check, validationResult } = require('express-validator');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/filmopediadb', {
+mongoose.connect( process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
