c889b3e187575338a2c1d9b71348a5e2b2092f69
danielvonboros <dboros40@gmail.com>
2021-06-07 12:50:27 +0200

    update models.js
    models.js
    -revert ES6 language module import, fix syntax error

--- models.js
+++ models.js
@@ -1,8 +1,8 @@
-import mongoose, { Schema as _Schema, model } from 'mongoose';
+const mongoose = require('mongoose');
 const {Schema} = mongoose;
-import { hashSync, compareSync } from 'bcrypt';
+const bcrypt = require('bcrypt');
 
-let movieSchema = _Schema({
+let movieSchema = mongoose.Schema({
   title: {type: String, required: true},
   description: {type: String, required: true},
   genre: {
@@ -18,27 +18,25 @@ let movieSchema = _Schema({
   featured: Boolean
 });
 
-let userSchema = _Schema({
+let userSchema = mongoose.Schema({
   username: {type: String, required: true},
   password: {type: String, required: true},
   email: {type: String, required: true},
   birthday: Date,
-  favoritemovies: [{type: _Schema.Types.ObjectId, ref: 'Movie'}]
+  favoritemovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 });
 
 userSchema.statics.hashPassword = (password) => {
-  return hashSync(password, 10);
+  return bcrypt.hashSync(password, 10);
 };
 
 userSchema.methods.validatePassword = function(password) {
   console.log(password);
-  return compareSync(password, this.Password);
+  return bcrypt.compareSync(password, this.password);
 };
 
-let Movie = model('Movie', movieSchema);
-let User = model('User', userSchema);
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
 
-const _Movie = Movie;
-export { _Movie as Movie };
-const _User = User;
-export { _User as User };
+module.exports.Movie = Movie;
+module.exports.User = User;
