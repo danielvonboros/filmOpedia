95c437b756e90eeb249c09f7706aa04de4133990
danielvonboros <dboros40@gmail.com>
2021-06-09 13:29:11 +0200

    remove auth method for /movies
    -index.js
    remove auth method for /movies

--- index.js
+++ index.js
@@ -38,11 +38,9 @@ app.use('/', express.static('public'));
 
 
 // Return a list of all movies to the user
-app.get('/movies'
-// , passport.authenticate('jwt', { session: false })
-, (req, res) => {
+app.get('/movies', function (req, res) {
   Movies.find()
-    .then((movies) => {
+    .then(function (movies) {
       res.status(201).json(movies);
     })
     .catch((err) => {
