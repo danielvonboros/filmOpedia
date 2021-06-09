cccf2f16818a5cf4e8b904ee9cc9eafe53eba0d8
danielvonboros <dboros40@gmail.com>
2021-06-09 16:30:53 +0200

    add allowedOrigins
    index.js
    -localhost:1234 added to allowedOrigins

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/" , (request , response)=> {
     response.send("welcome to the filmopedia API. For additional information on requests, navigate to '/documentation'");
   });
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
 
 app.use(cors({
   origin: (origin, callback) => {
